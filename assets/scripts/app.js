e4b97a1300bc1be3f8212f59224bec30592ad4b1
Akshay448 <akshaykapoor448@gmail.com>
2022-05-21 23:48:38 +0530

    basics 9

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,9 +2,8 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 function add() {
-  debugger;
-  currentResult = currentResult + userInput.value;
-  outputResult(currentResult, 'yei');
+  currentResult = currentResult + parseInt(userInput.value);
+  outputResult(currentResult, '');
 }
 
 addBtn.addEventListener('click', add);
